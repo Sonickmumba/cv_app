29767c194046b9a4532a856ddce620bec72eecb0
Sonick Mumba <kingellie.mumba@gmail.com>
2023-09-29 21:59:45 +0200

    Remove commented code

--- src/components/GeneralInfo.jsx
+++ src/components/GeneralInfo.jsx
@@ -25,12 +25,6 @@ const GeneralInfo = () => {
               </div>
               <button type='submit'>Submit</button>
             </form>
-            {/* <div>
-              <input type="text" value={name} onChange={(e) => setName(e.target.value)} />
-              <input type="text" value={email} onChange={(e) => setEmail(e.target.value)} />
-              <input type="number" value={email} onChange={(e) => setPhoneNumber(e.target.value)} />
-            </div>
-            <button type='submit' onClick={() => {setSubmit(true)}}>Submit</button> */}
           </>
         )}
       </div>
