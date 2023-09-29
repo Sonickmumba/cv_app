29cb90983bb7009748d782e5665a07cee4e84797
Sonick Mumba <kingellie.mumba@gmail.com>
2023-09-29 19:39:46 +0200

    Add practical component

---
+++ src/components/EducationExp.jsx
@@ -0,0 +1,9 @@
+// import React from 'react'
+
+// const EducationExp = () => {
+//   return (
+//     <div>EducationExp</div>
+//   )
+// }
+
+// export default EducationExp
---
+++ src/components/PracticalExp.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const PracticalExp = () => {
+  return (
+    <div>PracticalExp</div>
+  )
+}
+
+export default PracticalExp
